d5a2e0bd9f464ee481c2a555aa4a058c0e21e150
Ken Weed <kenneth.weed@instacart.com>
2018-12-02 22:39:32 -0800

    Fix create, add flash and error messages, crew show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: {min: 3, max: 50}
-    validates :description, presence: true, length: {min: 10, max: 300}
+    validates :title, presence: true, length: {minimum: 3, maximum: 50}
+    validates :description, presence: true, length: {minimum: 10, maximum: 300}
 
 end
