30c805e2a73d9badee4d8f30aeef93475068041e
Ken Weed <kenneth.weed@instacart.com>
2018-12-06 16:55:17 -0800

    Add secure passwords for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
+  
   before_save { self.email = email.downcase }
   validates :username, presence: true, 
     uniqueness: {case_sensitive: false}, 
@@ -10,4 +11,5 @@ class User < ApplicationRecord
     uniqueness: {case_sensitive: false},
     format: { with: VALID_EMAIL_REGEX}
 
+  has_secure_password
 end
