897145a8f6a7b4883ccb77739246d3c66060ac32
Ken Weed <kenneth.weed@instacart.com>
2018-12-09 23:35:02 -0800

    Add admin user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :articles
+  has_many :articles, dependent: :destory
   
   before_save { self.email = email.downcase }
   validates :username, presence: true, 
